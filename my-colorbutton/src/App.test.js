92897598eb4ccad5ddaf7c096a70e3c053f3cc75
faris <faris38kanbur@gmail.com>
2022-01-26 18:22:07 +0100

    configre eslint

--- my-colorbutton/src/App.test.js
+++ my-colorbutton/src/App.test.js
@@ -1,6 +1,6 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import App from './App';
-import { replacaCameWithSpaces } from './App'
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
+import { replacaCameWithSpaces } from "./App";
 
 // test('button has coorect initial color', () => {
 //   render(<App />);
@@ -32,25 +32,19 @@ import { replacaCameWithSpaces } from './App'
 //   const checkbox = screen.getByRole('checkbox');
 //   expect(checkbox).not.toBeChecked();
 
-
-
 // })
-
 
 // test('Checkbox disables button on first click and enables on seceond click', () => {
 //   render(<App/>);
 
 //   const colorButton = screen.getByRole('button', {name: 'Change to blue'});
 //   const checkbox = screen.getByRole('checkbox', {name: "Disable button"});
-  
 
 //   //on click checkbox to make unable colorButton
 //   fireEvent.click(checkbox)
 
 //   //expext unable check box
 //   expect(colorButton).toBeDisabled();
-
-  
 
 //   //click check box to make colorButton
 //   fireEvent.click(checkbox)
@@ -69,7 +63,6 @@ import { replacaCameWithSpaces } from './App'
 //   fireEvent.click(checkbox);
 //   expect(colorButton).toHaveStyle('backgroundColor:gray')
 
-
 //   fireEvent.click(checkbox);
 //   expect(colorButton).toHaveStyle('backgroundColor: red');
 // })
@@ -82,47 +75,45 @@ import { replacaCameWithSpaces } from './App'
 
 //   fireEvent.click(colorButton);
 
-
 //   fireEvent.click(checkbox);
-//   expect(colorButton).toHaveStyle('backgroundColor:gray');  
+//   expect(colorButton).toHaveStyle('backgroundColor:gray');
 
 //   fireEvent.click(checkbox);
 //   expect(colorButton).toHaveStyle('background-color: blue')
-  
+
 // });
 
-describe('spaces before camel-caser capital letter', ()=> {
-  test('Works for no inner capital ketter', () => {
-    expect(replacaCameWithSpaces('Red')).toBe('Red');
+describe("spaces before camel-caser capital letter", () => {
+  test("Works for no inner capital ketter", () => {
+    expect(replacaCameWithSpaces("Red")).toBe("Red");
   });
 
-  test('Works for one inner capital letter', () => {
-    expect(replacaCameWithSpaces('MidnightBlue')).toBe('Midnight Blue');
+  test("Works for one inner capital letter", () => {
+    expect(replacaCameWithSpaces("MidnightBlue")).toBe("Midnight Blue");
   });
 
-  test('Works for multiple inner capital letters', () => {
-    expect(replacaCameWithSpaces('MediumVioletRed')).toBe('Medium Violet Red');
-
+  test("Works for multiple inner capital letters", () => {
+    expect(replacaCameWithSpaces("MediumVioletRed")).toBe("Medium Violet Red");
   });
-  
 });
 
-test('button has coorect initial color', () => {
-  render(<App/>);
+test("button has coorect initial color", () => {
+  render(<App />);
 
   //find an element with a role of button and text of 'Change to Midnight Blue'
-  const colorButton = screen.getByRole('button', {name: 'Change to Midnight Blue'})
+  const colorButton = screen.getByRole("button", {
+    name: "Change to Midnight Blue",
+  });
 
   // expect the backgorund color to be red
-  expect(colorButton).toHaveStyle({backgroundColor: 'MediumVioletRed'})
+  expect(colorButton).toHaveStyle({ backgroundColor: "MediumVioletRed" });
 
   //click button
   fireEvent.click(colorButton);
 
   //expect the background color to be blue
-  expect(colorButton).toHaveStyle({backgroundColor: 'MidnightBlue'})
+  expect(colorButton).toHaveStyle({ backgroundColor: "MidnightBlue" });
 
   //expect the button text to be 'Changed to medium violet Red'
-  expect(colorButton.textContent).toBe('Change to Medium Violet Red')
-  
+  expect(colorButton.textContent).toBe("Change to Medium Violet Red");
 });
